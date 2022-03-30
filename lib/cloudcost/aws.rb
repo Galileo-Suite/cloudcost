54cbb73b59af84732675b57e37c0e9808f499d5b
Rich Davis <rdavis@galileosuite.com>
2022-03-30 18:26:05 -0400

    updates

--- lib/cloudcost/aws.rb
+++ lib/cloudcost/aws.rb
@@ -15,6 +15,25 @@ module AWS
     #
     CLIENT           = Aws::Pricing::Client.new
 
+    AWS_REGIONS      = {
+        "us-east-1":       "US East (N. Virginia)",
+        "us-east-2":       "US East (Ohio)",
+        "us-west-1":       "US West (N. California)",
+        "us-west-2":       "US West (Oregon)",
+        "us-gov-west-1":   "GovCloud (US Northwest)",
+        "ca-central-1":    "Canada (Montreal)",
+        "eu-west-1":       "EU (Ireland)",
+        "eu-west-2":       "EU (London)",
+        "eu-central-1":    "EU (Frankfurt)",
+        "ap-southeast-1":  "Asia Pacific (Singapore)",
+        "ap-southeast-2":  "Asia Pacific (Sydney)",
+        "ap-south-1":      "Asia Pacific (Mumbai)",
+        "ap-northeast-1":  "Asia Pacific (Tokyo)",
+        "ap-northeast-2":  "Asia Pacific (Seoul)",
+        "sa-east-1":       "South America (Saõ Paulo)",
+        "cn-north-1":      "China (Beijing)"
+    }
+
     # Get products is the pricing routine
     def get_products(options: nil, name: 'generic', limit: 1000, output_dir: nil)
         Log.info("Getting pricing for #{name}")
--- lib/cloudcost/version.rb
+++ lib/cloudcost/version.rb
@@ -2,4 +2,32 @@ module Cloudcost
 
   VERSION = "1.0.0"
 
+  SAMPLE_CONFIG = %Q(
+---
+
+# Azure client ID and Secret
+AZURE_CLIENTID: "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+AZURE_CLIENTSECRET: "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+
+# https://portal.azure.com/#blade/Microsoft_Azure_GTM/ModernBillingMenuBlade/Overview
+AZURE_SUBSCRIPTION: "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+# https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/Overview
+AZURE_TENENT: "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+
+# We will collect all us locations but when building the price
+# in the azure_build_price_file.rb script we will only get
+# eastus data. Uncomment others locations for more.
+AZURE_LOCATIONS:
+  eastus: "US East"
+  # westus: "US West"
+  # centralus: "US Central"
+  # eastus2: "US East 2"
+  # westus2: "US West 2"
+  # northcentralus: "US North Central"
+  # southcentralus: "US South Central"
+  # westcentralus: "US West Central"
+)
+
 end
